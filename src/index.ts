94abe44824b0f8a9d47b2a6bbfb80cfc1f6dcb37
MariyaN23 <sellaite505@gmail.com>
2024-09-16 18:28:30 +0300

    validation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import {productsRouter} from "./routes/products-route/products-router";
 import {addressesRouter} from "./routes/addresses-route/addresses-router";
 
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(express.json())
+
 app.use('/products', productsRouter)
 app.use('/addresses', addressesRouter)
-
-const parserMiddleware = bodyParser({})
-app.use(parserMiddleware)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- src/routes/products-route/products-router.ts
+++ src/routes/products-route/products-router.ts
@@ -1,57 +1,37 @@
-import {NextFunction, Request, Response, Router} from "express";
+import {Request, Response, Router} from "express";
 import {productsRepository} from "../../repositories/products-repository";
+import {body} from "express-validator";
+import {inputValidationMiddleware} from "../../middlewares/input-validation-middleware";
 
 export const productsRouter = Router({})
 
-//middleware
-const createdMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    // @ts-ignore
-    req.blabla = "Hello"
-    next()
-}
-const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    // @ts-ignore
-    if (req.query.token === "123") {
-        next()
-    } else {
-        res.send(401)
-    }
-}
-//if in query http://localhost:5000/products?token=123 - ok, next
-//else - Unauthorized
-
-let requestsCount = 0
-const requestsCountMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    requestsCount++
-    next()
-}
-productsRouter.use(requestsCountMiddleware) //count number of requests
-productsRouter.use(createdMiddleware)
-productsRouter.use(authGuardMiddleware)
+const titleValidation = body('title').trim().isLength({
+    min: 3,
+    max: 10
+}).withMessage("Title length should be from 3 to 10 symbols")
 
 productsRouter.get('/', (req: Request, res: Response) => {
-    // @ts-ignore
-    const blabla = req.blabla
-    res.send({value: blabla + "!!!!" + requestsCount})
-})
-/*productsRouter.get('/', (req: Request, res: Response) => {
     const foundProduct = productsRepository.findProduct(req.query.title?.toString())
     res.send(foundProduct)
-})*/
+})
 productsRouter.get('/:id', (req: Request, res: Response) => {
     const product = productsRepository.findProductById(+req.params.id)
     product ? res.send(product) : res.send(404)
 })
-productsRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = productsRepository.createProduct(req.body.title)
-    res.status(200).send(newProduct)
-})
-productsRouter.put('/:id', (req: Request, res: Response) => {
-    const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
-    isUpdated
-        ? res.send(productsRepository.findProductById(+req.params.id))
-        : res.send(404)
-})
+productsRouter.post('/',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const newProduct = productsRepository.createProduct(req.body.title)
+        res.status(201).send(newProduct)
+    })
+productsRouter.put('/:id',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+        isUpdated ? res.send(productsRepository.findProductById(+req.params.id)) : res.send(404)
+    })
 productsRouter.delete('/:id', (req: Request, res: Response) => {
     const isDeleted = productsRepository.deleteProduct(+req.params.id)
     isDeleted ? res.send(204) : res.send(404)
